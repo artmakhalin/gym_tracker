249bbdfa7ec2193f67918eef830df7938a58e15e
artmakhalin <artmakhalin@gmail.com>
2026-03-15 18:48:09 -0400

    init commit

---
+++ src/test/java/com/makhalin/gym_tracker/GymTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makhalin.gym_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
